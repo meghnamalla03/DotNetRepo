5c5890cc1e163f4d8d42ce16bb999c22d79f1ba9
Meghna Malla <meghna.malla@coxautoinc.com>
2024-07-21 22:37:02 +0530

    Adding Helloworld prog

---
+++ DotnetDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotnetDemo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("I am new");
+    }
+}
